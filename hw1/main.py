7d0c60523c061b311ffadf9a8333065ad3f73f55
Andrzej Piechowicz <andrzej.piechowicz.93@gmail.com>
2018-03-03 20:48:47 +0100

    [HW-1] Add solution for exercise 1 and 2

--- hw1/main.py
+++ hw1/main.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from matplotlib import pyplot
 from tqdm import tqdm
 
@@ -10,7 +12,8 @@ def main() -> None:
     input_dir, judgement_year = parse_input_dir_argument()
     numbers = get_number_data(input_dir, judgement_year)
     save_number_data(numbers)
-    create_histogram(numbers, "ex1", judgement_year)
+    exercise1(numbers, judgement_year)
+    exercise2(numbers, judgement_year)
 
 
 def get_number_data(input_dir, judgement_year):
@@ -38,10 +41,31 @@ def create_histogram(numbers: List[int], filename: str, year: int):
     output_file = get_absolute_path(OUTPUT_DIRECTORY_PATH, filename)
     pyplot.hist(numbers)
     pyplot.title('Number of occurrences of given money\namount across judgements from year {}'.format(str(year)))
-    pyplot.xlabel('Money amounts')
-    pyplot.ylabel('Number of occurrences')
+    pyplot.xlabel('Money amounts [pln]')
+    pyplot.ylabel('Number of occurrences [-]')
     pyplot.grid(True)
     pyplot.savefig(output_file, dpi=150)
+    pyplot.close()
+
+
+def exercise1(numbers: List[int], judgement_year: int):
+    create_histogram(numbers, "exercise-1", judgement_year)
+
+
+def exercise2(numbers: List[int], judgement_year: int):
+    a, b = split_list_by_value(numbers, 1000000)
+    create_histogram(a, 'exercise-2-upto-1m', judgement_year)
+    create_histogram(b, 'exercise-2-from-1m', judgement_year)
+
+
+def split_list_by_value(numbers: List[int], value: int) -> Tuple[List[int], List[int]]:
+    upto, above = [], []
+    for number in numbers:
+        if number > value:
+            above.append(number)
+        else:
+            upto.append(number)
+    return upto, above
 
 
 if __name__ == '__main__':
