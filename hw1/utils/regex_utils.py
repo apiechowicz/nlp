f6c3becbb290ebbd000d25a53bbf70c0e1d7f232
Andrzej Piechowicz <andrzej.piechowicz.93@gmail.com>
2018-03-03 16:40:06 +0100

    [HW-1] Update money pattern and add method for money string normalization

--- hw1/utils/regex_utils.py
+++ hw1/utils/regex_utils.py
@@ -3,7 +3,7 @@ from re import fullmatch, compile, IGNORECASE, findall
 
 INPUT_FILE_NAME_PATTERN = r'judgments-\d+\.json'
 JUDGEMENT_DATE_FORMAT = r'%Y-%m-%d'
-MONEY_PATTERN = r'(\d+(?:[ .,]?\d+)*(?: ?(?:tys\.|mln))?(?= ?(?:zł(?:ote|otych|(?!\w))|pln)))'
+MONEY_PATTERN = r'(?:[1-9]\d*|0(?=,))(?:[ .,]?\d+)*(?: ?(?:tys\.|mln))?(?= ?(?:zł(?:ote|otych|(?!\w))|pln))'
 
 
 def is_valid_input_file(filename: str) -> bool:
@@ -17,3 +17,30 @@ def judgement_year_matches(date: str, year: int) -> bool:
 def find_money_in_string(judgement_content: str):
     pattern = compile(MONEY_PATTERN, IGNORECASE)
     return findall(pattern, judgement_content)
+
+
+def convert_money_string_to_int(number: str) -> int:
+    number = __skip_grosze(number)
+    number = __replace_abbreviations(number)
+    number = __replace_delimiters(number)
+    return int(number)
+
+
+def __skip_grosze(number: str) -> str:
+    number_tuple = number.rsplit(',', 1)
+    if len(number_tuple) == 2 and len(number_tuple[1]) == 2:
+        return number_tuple[0]
+    return number
+
+
+def __replace_abbreviations(number: str) -> str:
+    number = number.replace('tys.', 3 * '0')
+    number = number.replace('mln', 6 * '0')
+    return number
+
+
+def __replace_delimiters(number: str) -> str:
+    number = number.replace(' ', '')
+    number = number.replace('.', '')
+    number = number.replace(',', '')
+    return number
