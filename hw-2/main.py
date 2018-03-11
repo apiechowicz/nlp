05d8a738b4236c9471abdbd392cbbaf802f99a7b
Andrzej Piechowicz <andrzej.piechowicz.93@gmail.com>
2018-03-11 22:53:26 +0100

    [HW-2] Replace separate index and analyzer creation with creating index with analyzer

--- hw-2/main.py
+++ hw-2/main.py
@@ -3,35 +3,27 @@ from os import getcwd
 import requests
 
 from utils.argument_parser import parse_arguments
-from utils.file_utils import get_create_analyzer_json, get_create_index_json, get_files_to_be_processed, \
+from utils.file_utils import get_create_index_json, get_files_to_be_processed, \
     extract_and_upload_data
 
 HOST = r'http://localhost:9200'
-ANALYZER = HOST + r'/nlp_analyzer'
-INDEX = HOST + r'/nlp_index'
+INDEX = HOST + r'/nlp-hw2'
 INDEX_DATA = INDEX + r'/judgements'
 HEADERS = {'content-type': 'application/json'}
 
 
 def main():
     input_dir, judgement_year = parse_arguments()
-    create_analyzer()
-    create_index()
+    print(create_index_with_analyzer())
     files = get_files_to_be_processed(input_dir)
     for file in files:
         extract_and_upload_data(file, judgement_year, INDEX_DATA, HEADERS)
 
 
-def create_analyzer() -> int:
-    data = get_create_analyzer_json(getcwd())
-    response = requests.put(url=ANALYZER, headers=HEADERS, data=data)
-    return response.status_code
-
-
-def create_index() -> int:
+def create_index_with_analyzer() -> str:
     data = get_create_index_json(getcwd())
     response = requests.put(url=INDEX, headers=HEADERS, data=data)
-    return response.status_code
+    return response.content
 
 
 if __name__ == '__main__':
--- hw-2/utils/file_utils.py
+++ hw-2/utils/file_utils.py
@@ -8,14 +8,9 @@ from typing import List, Dict
 import requests
 
 JSONS_DIRECTORY = r'jsons'
-CREATE_ANALYZER_JSON = r'2-create-analyzer.json'
-CREATE_INDEX_JSON = r'3-create-index.json'
+CREATE_INDEX_JSON = r'create-index-with-analyzer.json'
 INPUT_FILE_NAME_PATTERN = r'judgments-\d+\.json'
 JUDGEMENT_DATE_FORMAT = r'%Y-%m-%d'
-
-
-def get_create_analyzer_json(working_dir: str) -> str:
-    return __read_json_file_to_string(join(working_dir, JSONS_DIRECTORY, CREATE_ANALYZER_JSON))
 
 
 def get_create_index_json(working_dir: str) -> str:
