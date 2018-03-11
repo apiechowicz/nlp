12de251f53bce6af4a5e52444b63485ad57bf07f
Andrzej Piechowicz <andrzej.piechowicz.93@gmail.com>
2018-03-11 19:22:16 +0100

    [HW-2] Add index creation

--- hw-2/main.py
+++ hw-2/main.py
@@ -1,23 +1,30 @@
 from os import getcwd
-from os.path import join
 
 import requests
 
-from utils.file_utils import JSONS_DIRECTORY, CREATE_ANALYZER_JSON, read_json_to_string
+from utils.file_utils import get_create_analyzer_json, get_create_index_json
 
 HOST = r'http://localhost:9200'
 ANALYZER = HOST + r'/nlp_analyzer'
+INDEX = HOST + r'/nlp_index'
 
 
 def main():
     create_analyzer()
+    create_index()
 
 
 def create_analyzer() -> int:
-    json_path = join(getcwd(), JSONS_DIRECTORY, CREATE_ANALYZER_JSON)
-    data = read_json_to_string(json_path)
+    data = get_create_analyzer_json(getcwd())
     headers = {'content-type': 'application/json'}
     response = requests.put(url=ANALYZER, headers=headers, data=data)
+    return response.status_code
+
+
+def create_index() -> int:
+    data = get_create_index_json(getcwd())
+    headers = {'content-type': 'application/json'}
+    response = requests.put(url=INDEX, headers=headers, data=data)
     return response.status_code
 
 
