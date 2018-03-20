2e87ea5dc5db633efb8708ef627f1fc1a8b7f3de
Andrzej Piechowicz <andrzej.piechowicz.93@gmail.com>
2018-03-20 23:22:09 +0100

    [HW-4] Add creation of frequency list

--- hw-4/main.py
+++ hw-4/main.py
@@ -1,5 +1,43 @@
+from operator import itemgetter
+
+from tqdm import tqdm
+
+from utils.argument_parser import *
+from utils.file_utils import *
+from utils.regex_utils import replace_all_redundant_characters
+
+
 def main():
-    pass
+    input_dir, judgement_year = parse_arguments()
+    files = get_files_to_be_processed(input_dir)
+    results = process_data(files, judgement_year)
+    results = sort_dictionary(results)
+    save_data(results, 'exercise-1.txt')
+    # results = read_data('exercise-2.txt')
+
+
+def process_data(files: List[str], judgement_year: int) -> Dict[str, int]:
+    words_with_number_of_occurrences = dict()
+    for file in tqdm(files, mininterval=15, unit=' files'):
+        judgements = extract_judgements_from_given_year_from_file(file, judgement_year)
+        for judgement in judgements:
+            content = extract_from_judgement(judgement, 'textContent')
+            content = replace_all_redundant_characters(content)
+            for word in content.split():
+                if is_not_a_number(word) and is_a_word(word):
+                    update_dictionary(words_with_number_of_occurrences, word.lower())
+    return words_with_number_of_occurrences
+
+
+def update_dictionary(words_with_number_of_occurrences: Dict[str, int], word: str) -> None:
+    try:
+        words_with_number_of_occurrences[word] += 1
+    except KeyError:
+        words_with_number_of_occurrences[word] = 1
+
+
+def sort_dictionary(results: Dict[str, int]):
+    return sorted(results.items(), key=itemgetter(1), reverse=True)
 
 
 if __name__ == '__main__':
